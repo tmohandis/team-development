4bcd89b38ff8822cddb06880d348d4625565ae72
tmohandis <39660548+tmohandis@users.noreply.github.com>
2020-03-12 17:37:36 +0300

    Delete params.php

--- config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'geekteamdev@yandex.ru',
-    'senderEmail' => 'geekteamdev@yandex.ru',
-    'senderName' => 'Example.com mailer',
-    'supportEmail' => 'geekteamdev@yandex.ru',
-    'user.passwordResetTokenExpire' => 3600,
-];
