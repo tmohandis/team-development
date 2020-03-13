fc08025127b1326bc994fb93770e10d3ad764fb9
Nazarov1987 <ns-nazarov@mail.ru>
2020-03-13 12:53:45 +0300

    Установлен nested sets behavior. Отредактированы миграции category_table в соответствии с требованиями nested sets behavior. Создана модель Сategory. Создан контроллер CRUD модели для Сategory. Настрен СategoryController и view. Установлен yii2-fancytree-widget. Установлен NestedSetsTreeBehavior. Пока проблемы с отображением дерева категорий.

---
+++ views/category/tree.php
@@ -0,0 +1,46 @@
+<?php
+
+use app\models\Category;
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+use yii\web\JsExpression;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CategorySearch2 */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Дерево категорий';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<pre><?php ?></pre>
+
+<?php
+
+try {
+    echo \wbraganca\fancytree\FancytreeWidget::widget([
+        'options' => [
+            'source' => $data,
+            'extensions' => ['dnd'],
+            'dnd' => [
+                'preventVoidMoves' => true,
+                'preventRecursiveMoves' => true,
+                'autoExpandMS' => 400,
+                'dragStart' => new JsExpression('function(node, data) {
+                    return true;
+                }'),
+                'dragEnter' => new JsExpression('function(node, data) {
+                    return true;
+                }'),
+                'dragDrop' => new JsExpression('function(node, data) {
+                    data.otherNode.moveTo(node, data.hitMode);
+                }'),
+            ],
+        ]
+    ]);
+} catch (Exception $e) {
+}
+
+
+
